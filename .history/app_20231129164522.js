47f3c732807d292278930e0dc5b280319a8f6eab
th3-christopher <89815443+th3-christopher@users.noreply.github.com>
2023-11-29 16:45:53 -0700

    permissions 4

--- .history/app_20231129164522.js
+++ .history/app_20231129164522.js
@@ -35,7 +35,7 @@ const authenticated_menu=[
         {label:"Personal Data",function:"navigate({fn:'personal_data'})"},
     ]},
     //This menu item allows the user to logout
-    {label:"Logout",function:"logout()", home:"Logout", roles:["owner","administrator","employee", "user", "shift lead"]},
+    {label:"Logout",function:"logout()", home:"Logout"},
     //This menu item builds a sub menu that provides the user with the functionality to request time off and see their requests
     //{label:"Time Off",id:"menu1",menu:[
     //    {label:"Request Time Off",function:"navigate({fn:'request_time_off'})"}, 
